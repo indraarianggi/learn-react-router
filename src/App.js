ff11744c63c7c6ceb7ac11e560f2587a2f4341ef
indraarianggi <indraarianggi@gmail.com>
2019-01-06 11:56:18 +0700

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Link, Route, Switch } from "react-router-dom";
 
+/* Home Component */
+const Home = () => (
+    <div>
+        <h2>Home</h2>
+    </div>
+);
+
+/* Category Component */
+const Category = () => (
+    <div>
+        <h2>Category</h2>
+    </div>
+);
+
+/* Products Component */
+const Products = () => (
+    <div>
+        <h2>Products</h2>
+    </div>
+);
+
+/* App Component */
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/category">Category</Link>
+                        </li>
+                        <li>
+                            <Link to="/products">Products</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/category" component={Category} />
+                    <Route path="/products" component={Products} />
+                    <Route
+                        path="/:id"
+                        render={() => (
+                            <p>
+                                {" "}
+                                I want this text to show up for all routes other
+                                than '/', '/products' and '/category'{" "}
+                            </p>
+                        )}
+                    />
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
